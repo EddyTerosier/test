9a4f0449cb056bdb239a00f36247d0cbb97a6605
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-21 14:02:31 +0100

    FUNCTION AVEC LE STATE NAVLINK COLOR

--- src/Composants/main/Header.js
+++ src/Composants/main/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <nav class="navbar navbar-expand-md navbar-light bg-custom">
         <div class="container">
-        <Link to="/"><a onClick={activeRemove} class="navbar-brand" href="Index.html"><img src={Logo} alt="Logo" width="115px"/></a></Link>
+        <Link to="/"><a onClick={activeState} class="navbar-brand" href="Index.html"><img src={Logo} alt="Logo" width="115px"/></a></Link>
             <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                 aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -14,13 +14,13 @@ export default function Header() {
             <div class="collapse navbar-collapse" id="collapsibleNavId">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <Link to="/"><a id = "1" class="nav-link active" aria-current="page" onClick={activeRemove}>Accueil</a></Link>
+                        <Link to="/"><a id = "1" class="nav-link active" aria-current="page" onClick={activeState}>Accueil</a></Link>
                     </li>
                     <li class="nav-item">
-                        <a id = "2" class="nav-link" href="#" onClick={activeRemove}>Concept</a>
+                        <a id = "2" class="nav-link" href="#" onClick={activeState}>Concept</a>
                     </li>
                     <li class="nav-item">
-                    <Link to="/Programmes"><a id = "3" class="nav-link" href="#" onClick={activeRemove}>Programmes</a></Link>
+                    <Link to="/Programmes"><a id = "3" class="nav-link" href="#" onClick={activeState}>Programmes</a></Link>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" id="dropdownId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Giga+</a>
@@ -39,11 +39,12 @@ export default function Header() {
     </nav>
   )
 }
-const element1 = document.getElementById("1");
-const element2 = document.getElementById("2");
-const element3 = document.getElementById("3");
-const active = document.querySelector(".active");
-const elements = element1+element2+element3;
+
+// const element1 = document.getElementById("1");
+// const element2 = document.getElementById("2");
+// const element3 = document.getElementById("3");
+// const active = document.querySelector(".active");
+// const elements = element1+element2+element3;
 
 // FONCTION CLASSIQUE
 
@@ -57,4 +58,16 @@ const elements = element1+element2+element3;
 
 // FONCTION FLECHEE + TERNAIRE
 
-const activeRemove = () => {!active ? elements.classList.add("active") : elements.classList.remove("active")};
+// const activeRemove = () => {!active ? elements.classList.add("active") : elements.classList.remove("active")};
+
+// FONCTION AVEC STATE 
+
+const activeState = (e)=>{
+    const id = e.target.id
+    const idArray = ["1","2","3"]
+
+    idArray.forEach((element)=> {
+        document.getElementById(element).classList.remove("active")
+    });
+    document.getElementById(id).classList.add("active")
+}
